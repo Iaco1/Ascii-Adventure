e9b917aa286a88b39cfab98b388f1c0d7a42dd8e
Iaco1 <1iacominodavide@gmail.com>
2021-05-26 10:01:36 +0200

    added Level and Map classes (to Be Implemented)

---
+++ include/Map.hpp
@@ -0,0 +1,10 @@
+#include "LinkedList.hpp"
+#include "Level.hpp"
+
+class Map{
+    protected:
+    LinkedList<Level> map;
+    public:
+    Map(int size);      //generates a map list of size levels
+    void printMap();    //prints each level  
+};
