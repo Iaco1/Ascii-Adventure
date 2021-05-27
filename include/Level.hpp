290c7b483283a493575997c320f98a815b9da29d
TheMancio <00lm@protonmail.ch>
2021-05-27 23:50:33 +0200

    Level.cpp: implemented Level() and to_string(); Level.hpp: changed TileChar[]'s type to char

--- include/Level.hpp
+++ include/Level.hpp
@@ -1,14 +1,14 @@
 #include "Menu.hpp"
 
 //size is the no. elements in TileType
-enum class TileType{ EMPTY, TERRAIN, HERO, ENEMY, BONUS, MALUS, SIZE };
-const char *TileChar[]{ " ", "#", "H", "E", "B", "M"};
+enum class TileType { EMPTY, TERRAIN, HERO, ENEMY, BONUS, MALUS, SIZE };
+const char TileChar[]{ ' ', '#', 'H', 'E', 'B', 'M' };
 
-class Level{
-    protected:
+class Level {
+protected:
     TileType Board[WINDOW_HEIGHT][WINDOW_WIDTH];
-    public:
+public:
     Level();                    //fills the board of TileType::EMPTY enums
-    Level(int fillPercentage);  //fills fillPercentage % of the board randomly with TileTypes enums
-    char *to_string();          //returns a char* representing how the map looks like
+    Level(double fillPercentage);  //fills fillPercentage % of the board randomly with TileTypes enums
+    char* to_string();          //returns a char* representing how the map looks like
 };
--- src/Level.cpp
+++ src/Level.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 #include "Level.hpp"
+
+using namespace std;
+
+Level::Level() {
+    for (int i = 0; i < WINDOW_HEIGHT; i++) {
+        for (int j = 0; j < WINDOW_WIDTH; j++)
+            Board[i][j] = TileType::EMPTY;
+    }
+}
+
+Level::Level(double fillPercentage) {
+    //tba
+}
+
+char* Level::to_string() {
+    char* levelOutput = new char[WINDOW_HEIGHT * (WINDOW_WIDTH + 1)];
+    int count = 0;
+    for (int i = 0; i < WINDOW_HEIGHT; i++, count++) {
+        for (int j = 0; j < WINDOW_WIDTH; j++, count++)
+            levelOutput[count] = TileChar[(int)Board[i][j]];
+        levelOutput[count] = count == WINDOW_HEIGHT * (WINDOW_WIDTH + 1) - 1 ? '\0' : '\n';
+    }
+    return levelOutput;
+}
