87a26f51a8fa71f202e2c949ef60d6db1e94786a
Iaco1 <1iacominodavide@gmail.com>
2021-12-03 14:40:26 +0100

    made Action into a class and added animationDelay and Initiator enum classes

---
+++ include/Action.hpp
@@ -0,0 +1,23 @@
+#include <chrono>
+
+enum class Animation{CLIMB_UP, CLIMB_DOWN, LEFT, RIGHT, STILL, JUMPING, FALLING};
+enum class AnimationDelay{ CLIMB_UP = 0, CLIMB_DOWN = 0, LEFT = 0, RIGHT = 0, STILL = 0, JUMPING = 100, FALLING = 100};
+enum class Initiator{ USER, LOGIC};
+
+class Action{
+    protected:
+    Animation animation;
+    int x, y; //affected position
+    std::chrono::milliseconds delay;
+    Initiator initiator;
+
+    public:
+    Action();
+    Action(Animation animation, int x, int y, AnimationDelay ec_delay, Initiator initiator);
+    Action(Animation animation, int x, int y, int delay, Initiator initiator);
+    Animation getAnimation();
+    int getX();
+    int getY();
+    int getDelay();
+    Initiator getInitiator();
+};
