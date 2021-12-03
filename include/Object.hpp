a924c335758dbe4126e6a133f22472e05ae263f5
Iaco1 <1iacominodavide@gmail.com>
2021-12-03 14:38:14 +0100

    corrected tiletype casing to tileType and getters for x, y

--- include/Object.hpp
+++ include/Object.hpp
@@ -3,14 +3,16 @@ enum class TileType{ EMPTY, TERRAIN, HERO, ENEMY, BONUS, MALUS, SIZE};
 class Object{
     protected:
     int x, y;
-    TileType tiletype;
+    TileType tileType;
     
     public:
     Object();
-    Object(int x, int y, TileType tiletype);
+    Object(int x, int y, TileType tileType);
     Object(bool random); //creates a random object
     void toString(char str[24]);
     void getXY(int &x, int &y);
+    int getX();
+    int getY();
     char getTileChar();
     void setXY(int x, int y);
     TileType getTileType();
--- src/Object.cpp
+++ src/Object.cpp
@@ -6,14 +6,14 @@
 Object::Object(){
         x=0;
         y=0;
-        tiletype = TileType::EMPTY;
+        tileType = TileType::EMPTY;
     }
 
-Object::Object(int x, int y, TileType tiletype){
+Object::Object(int x, int y, TileType tileType){
         if(x>=0 && y>=0){
             this->x = x;
             this->y = y;
-            this->tiletype = tiletype;
+            this->tileType = tileType;
         }else{
             Object();
         }
@@ -23,13 +23,13 @@ Object::Object(bool random){
     if(random==true){
 			x = rand()%100;
 			y = rand()%20;
-			tiletype = static_cast<TileType>(rand()%(static_cast<int>(TileType::SIZE)));
+			tileType = static_cast<TileType>(rand()%(static_cast<int>(TileType::SIZE)));
 	}else Object();
 }
 
 void Object::toString(char str[24]){
 	const char* tileTypeStr[] = {"Empty", "Terrain", "Hero", "Enemy", "Bonus", "Malus"};
-    sprintf(str, "((%d,%d),%s)", x, y, tileTypeStr[(int)tiletype]);
+    sprintf(str, "((%d,%d),%s)", x, y, tileTypeStr[(int)tileType]);
 }
 
 void Object::getXY(int &x, int &y){
@@ -39,7 +39,7 @@ void Object::getXY(int &x, int &y){
 
 char Object::getTileChar(){
     char tileChar[]{ ' ', '#', 'H', 'E', 'B', 'M' };
-    return tileChar[(int)tiletype];
+    return tileChar[(int)tileType];
 }
 
 void Object::setXY(int x, int y){
@@ -49,4 +49,7 @@ void Object::setXY(int x, int y){
     }
 }
 
-TileType Object::getTileType(){ return tiletype; }
+TileType Object::getTileType(){ return tileType; }
+
+int Object::getX(){ return x; }
+int Object::getY(){ return y; }
