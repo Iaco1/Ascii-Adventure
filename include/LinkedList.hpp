bdefc7f56bff1e1b4642427f0fc77101f0442440
TheMancio <00lm@protonmail.ch>
2021-12-06 02:28:03 +0100

    Small tweaks to LinkedList.hpp; added method popNode()

--- include/LinkedList.hpp
+++ include/LinkedList.hpp
@@ -2,175 +2,208 @@
 using namespace std;
 
 template <class T>
-class Node{
-	public:
+class Node {
+public:
 	T data;
-	Node *next;
-	Node *prev;
-	Node(){
+	Node* next;
+	Node* prev;
+	Node() {
 		data = T();
 		next = NULL;
 		prev = NULL;
 	}
-	Node(T data){
-		this->data = data;
+	Node(T d) {
+		data = d;
 		prev = NULL;
 		next = NULL;
+	}
+	~Node() {
+		if (prev != NULL) { prev->next = next; }
+		if (next != NULL) { next->prev = prev; }
 	}
 };
 
 template <class T>
-class LinkedList{
-	private:
-	Node<T> *head;
-	Node<T> *tail;
+class LinkedList {
+private:
+	Node<T>* head;
+	Node<T>* tail;
 	int size;
-	
-	public:
-	LinkedList(){
+
+public:
+	LinkedList() {
 		head = NULL;
 		tail = NULL;
 		size = 0;
 	}
-	Node<T>* getHead(){ return head; }
-	Node<T>* getTail(){ return tail; }
-	void pushHead(Node<T> *n){
-		if(head == NULL) {
-			head = n;
-			tail = n;
-		}else if(head == tail){
-			head = n;	
-			head->next = tail;
-			tail->prev = head;
-		}else{
-			n->next = head;
-			head->prev = n;
-			head = n;
-		}
+	Node<T>* getHead() { return head; }
+
+	Node<T>* getTail() { return tail; }
+
+	void pushHead(Node<T>* n) {
+		n->next = head;
+		head = n;
+		if (n->next != NULL) n->next->prev = n;
+		else tail = head;
 		size++;
 	}
-	void pushTail(Node<T> *n){
-		if(tail==NULL){
-			head = n;
-			tail = n;
-		}else if(head == tail){
-			head->next = n;
-			n->prev = head;
-			tail = n;
-		}else{
-			tail->next = n;
-			n->prev = tail;
-			tail = n;
-		}
+
+	void pushTail(Node<T>* n) {
+		n->prev = tail;
+		tail = n;
+		if (n->prev != NULL) n->prev->next = n;
+		else head = tail;
 		size++;
 	}
-	void pushIndex(Node<T> *n, int i){
-		Node<T> *iter = head;
-		if(i==0) pushHead(n);
-		else if(i == size) pushTail(n);
-		else if(i<size && i>0){
-			while(i>0){
+
+	void pushIndex(Node<T>* n, int i) {
+		if (i == 0) pushHead(n);
+		else if (i == size) pushTail(n);
+		else if (i < size && i > 0) {
+			Node<T>* iter = head;
+			while (i > 0) {
 				iter = iter->next;
 				i--;
 			}
-			Node<T> *previous = iter->prev;
-			previous->next = n;
-			n->prev = previous;
 			n->next = iter;
+			n->prev = iter->prev;
 			iter->prev = n;
+			n->prev->next = n;
 			size++;
 		}
 	}
-	void printList(){
-		Node<T> *iter=head;
-		cout<<endl<<"NULL<-";
-		while(iter != NULL){
+
+	void printList() {
+		Node<T>* iter = head;
+		cout << endl << "NULL<-";
+		while (iter != NULL) {
 			char str[24];
 			iter->data.toString(str);
 			printf("[%s]", str);
 			iter = iter->next;
-			if(iter!=NULL) cout<<"<->";
+			if (iter != NULL) cout << "<->";
 		}
-		cout<<"->NULL"<<endl;
+		cout << "->NULL" << endl;
 	}
-	void popHead(){
-		if(size>1){
-			head = head->next;
-			head->prev = NULL;
-			size--;
-		}else if(size == 1) {
-			head = NULL;
-			tail = NULL;
-			size--;
+
+	Node<T>* popHead(bool cleanup = true) {
+		size--;
+		Node<T>* tmp = head;
+		head = head->next;
+		if (head != NULL) head->prev = NULL;
+		else tail = NULL;
+		if (cleanup) {
+			delete n;
+			return NULL;
 		}
-	}
-	void popTail(){
-		if(size>1){
-			tail = tail->prev;
-			tail->next = NULL;
-			size--;
-		}else if(size == 1) {
-			tail = NULL;
-			head = NULL;
-			size--;
+		else {
+			tmp->prev = NULL;
+			tmp->next = NULL;
+			return tmp;
 		}
 	}
-	void popIndex(int i){
-		Node<T> *iter = head;
-		if(size>0){
-			if(i==0) popHead();
-			else if(i==size-1) popTail();
-			else if(i>0 && i<size-1){
-				while(i>0){
+
+	Node<T>* popTail(bool cleanup = true) {
+		size--;
+		Node<T>* tmp = tail;
+		tail = tail->prev;
+		if (tail != NULL) tail->next = NULL;
+		else head = NULL;
+		if (cleanup) {
+			delete n;
+			return NULL;
+		}
+		else {
+			tmp->prev = NULL;
+			tmp->next = NULL;
+			return tmp;
+		}
+	}
+
+	Node<T>* popIndex(int i, bool cleanup = true) {
+		Node<T>* iter = head;
+		if (size > 0) {
+			if (i == 0) popHead();
+			else if (i == size - 1) popTail();
+			else if (i > 0 && i < size - 1) {
+				while (i > 0) {
 					iter = iter->next;
 					i--;
 				}
-				Node<T> *iter_prev = iter->prev;
-				Node<T> *iter_next = iter->next;
-				iter_prev->next = iter_next;
-				iter_next->prev = iter_prev;
-				iter->prev = NULL;
-				iter->next = NULL;
+				iter->prev->next = iter->next;
+				iter->next->prev = iter->prev;
 				size--;
+				if (cleanup) {
+					delete n;
+					return NULL;
+				}
+				else {
+					iter->prev = NULL;
+					iter->next = NULL;
+					return iter;
+				}
 			}
 		}
+		else return NULL;
 	}
-	Node<T>* getNodeAt(int i){
-		if((i>0 || i==0) && i<size){
-			if(i==0) return head;
-			else if(i == size-1) return tail;
-			else{
-				Node<T> *iter = head;
-				while(i>0){
+
+	Node<T>* popNode(Node<T>* n, bool cleanup = true) {
+		if (contains(n)) {
+			if (n->next != NULL) n->next->prev = n->prev;
+			else head = n->next;
+			if (n->prev != NULL) n->prev->next = n->next;
+			else tail = n->prev;
+			size--;
+			if (cleanup) {
+				delete n;
+				return NULL;
+			}
+			else {
+				n->prev = NULL;
+				n->next = NULL;
+				return n;
+			}
+		}
+		else return NULL;
+	}
+
+	Node<T>* getNodeAt(int i) {
+		if (i >= 0 && i < size) {
+			if (i == 0) return head;
+			else if (i == size - 1) return tail;
+			else {
+				Node<T>* iter = head;
+				while (i > 0) {
 					iter = iter->next;
 					i--;
 				}
 				return iter;
 			}
-		}else return NULL;
+		}
+		else return NULL;
 	}
-	bool contains(Node<T> *n){
-		Node<T> *iter = head;
-		while(iter!=NULL){
-			if(iter == n) return true;
+
+	bool contains(Node<T>* n) {
+		Node<T>* iter = head;
+		while (iter != NULL) {
+			if (iter == n) return true;
 			else iter = iter->next;
 		}
 		return false;
 	}
-	int getIndex(Node<T> *n){
-		int i=0;
-		if(contains(n)){
-			Node<T> *iter = head;
-			while(iter!=NULL){
-				if(iter == n) return i;
-				else{
-					i++;
-					iter = iter->next;
-				}
-			}
-		}else return -1;
+
+	int getIndex(Node<T>* n) {
+		int i = 0;
+		Node<T> iter = head;
+		while (iter != NULL) {
+			if (iter == n) return i;
+			else { i++; iter = iter->next; }
+		}
+		return -1;
 	}
-	int getSize(){ return size; }
+
+	int getSize() { return size; }
+
 	T& operator[](int index) {
 		if (index >= size || index < 0) {
 			cout << "Error: index out of bounds" << endl;
@@ -187,9 +220,13 @@ class LinkedList{
 		}
 		return iter->data;
 	}
-	~LinkedList(){
-		head = NULL;
-		tail = NULL;
-		size = 0;
+
+	~LinkedList() {
+		Node<T>* iter = head;
+		while (iter != NULL) {
+			Node<T>* tmp = iter;
+			iter = iter->next;
+			delete tmp;
+		}
 	}
 };
