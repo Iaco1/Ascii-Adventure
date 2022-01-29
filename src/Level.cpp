e24c91e881eb0330a93f0139bb340aa1f3daf23c
TheMancio <00lm@protonmail.ch>
2022-01-29 18:00:36 +0100

    Changed number of enemies spawned

--- src/Level.cpp
+++ src/Level.cpp
@@ -10,35 +10,18 @@ Level::Level(int w, int h, int levelIndex) {
 
     //previous level door placement
     if (levelIndex > 0) prevLevelDoor = Object(1, h - 1, TileType::PL_DOOR);
-    else prevLevelDoor = Object(); //this has TileType::EMPTY and thus should not be drawn
+    else prevLevelDoor = Object();
 
     //floor generation
     for (int i = 0; i <= w; i++) terrain.pushHead(new Node<Object>(Object(i, h, TileType::TERRAIN)));
 
     //platform generation
-    //creates one 3-character long platform
-    //for (int i = 0; i < 6; i++) terrain.pushHead(new Node<Object>(Object(1 + i, h - 3, TileType::TERRAIN)));
-
-    //bonuses.pushHead(new Node<Bonus>(Bonus(3, h - 1, 100, BonusType::HP, 100)));
-    //bonuses.pushHead(new Node<Bonus>(Bonus(5, h - 1, 100, BonusType::AMMO, 10)));
-    //bonuses.pushHead(new Node<Bonus>(Bonus(7, h - 1, 100, BonusType::MAXAMMO, 100)));
-    //bonuses.pushHead(new Node<Bonus>(Bonus(9, h - 1, 100, BonusType::INSTAKILL, 1)));
-
-    //maluses.pushHead(new Node<Malus>(Malus(w / 2, h - 1, 100, 5, MalusType::THORN, 10)));
-    //maluses.pushHead(new Node<Malus>(Malus(w / 2 + 2, h - 1, 100, 50, MalusType::LANDMINE, 1)));
-
-    //maluses.pushHead(new Node<Malus>(Malus(w / 2 + 4, h - 1, 100, 1, MalusType::BARBED_WIRE, 20)));
-    //maluses.pushHead(new Node<Malus>(Malus(w / 2 + 4, h - 2, 100, 1, MalusType::BARBED_WIRE, 20)));
-    //maluses.pushHead(new Node<Malus>(Malus(w / 2 + 4, h - 3, 100, 1, MalusType::BARBED_WIRE, 20)));
-
-    //enemies.pushHead(new Node<Enemy>(Enemy(w - 1, h - 1, 100, 30, EnemyType::SENTRY)));
-    //xps.pushHead(new Node<Object>(Object(5, h - 4, TileType::XP)));
-
     generatePlatforms(vertBound - 4, horBound / 2, 0, horBound - 1, 1);
 
     //next level door placement
     generateNLDoor();
-    //nextLevelDoor = Object(5, h - 1, TileType::NL_DOOR);
+
+    //entities generation
     spawnEnemies(levelIndex);
     spawnBonuses(levelIndex);
     spawnMaluses(levelIndex);
@@ -290,13 +273,12 @@ void Level::findFreeSpace(int& x, int& y, int offset) {
 }
 
 void Level::spawnEnemies(int currentLevel) {
-    int enemiesNum = Misc::diceDistribution(2, Misc::bound(currentLevel + 3, 4, 20), Misc::bound(currentLevel + 3, 4, 20) / 2, 3);
+    int enemiesNum = Misc::diceDistribution(3, Misc::bound(currentLevel + 5, 6, 20), Misc::bound(currentLevel + 5, 6, 20) / 2, 3);
     for (int i = 0; i < enemiesNum; i++) {
         int x, y;
         findFreeSpace(x, y);
         if (x == -1 || y == -1) break;
         enemies.pushHead(new Node<Enemy>(Enemy(x, y, 100, 30, (EnemyType)Misc::randInt(0, 1))));
-        //tba: make enemies stronger
     }
 }
 
