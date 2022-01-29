ead06eb8a70abccc388da04e29c25528a33383be
TheMancio <00lm@protonmail.ch>
2022-01-29 02:27:57 +0100

    Added animated welcome screen (wip title)

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -5,26 +5,62 @@
 
 using namespace std;
 
-Menu::Menu(){
-    
+Menu::Menu() {
+
 }
 
 //prints the title
-WINDOW* Menu::printStartScreen(){
-    int title_w = 24;
+WINDOW* Menu::printStartScreen() {
+    clear();
+    refresh();
+    init_pair(1, COLOR_BLUE, COLOR_BLACK);
+    int title_h = 10;
+    int title_w = 46;
     int h, w;
     getmaxyx(stdscr, h, w);
-    WINDOW* title = newwin(3, title_w+2, (h/2)-3,w/2 - title_w/2);
-    WINDOW* belowTitle = newwin(3,30+2, h-4,w/2-16); // used to write the press space to play
+    WINDOW* title = newwin(title_h, title_w, h / 2 - title_h / 2 - 3, w / 2 - title_w / 2);
+    WINDOW* belowTitle = newwin(3, 30 + 2, h - 4, w / 2 - 16); // used to write the press space to play
 
-    mvwprintw(title,1,1, "Basic 2D ASCII Platform");
-    wrefresh(title);
-    
     curs_set(0);
-    sleep(2);
+    wattron(title, COLOR_PAIR(1));
+    mvwprintw(title, 0, 0, " ______           _                    _    _ ");
+    wrefresh(title);
+    refresh();
+    usleep(300000);
+    mvwprintw(title, 1, 0, "(_____ \\         (_)           _      \\ \\  / /");
+    wrefresh(title);
+    refresh();
+    usleep(300000);
+    mvwprintw(title, 2, 0, " _____) )___ ___  _  ____ ____| |_     \\ \\/ / ");
+    wrefresh(title);
+    refresh();
+    usleep(300000);
+    mvwprintw(title, 3, 0, "|  ____/ ___) _ \\| |/ _  ) ___)  _)     )  (  ");
+    wrefresh(title);
+    refresh();
+    usleep(300000);
+    mvwprintw(title, 4, 0, "| |   | |  | |_| | ( (/ ( (___| |__    / /\\ \\ ");
+    wrefresh(title);
+    refresh();
+    usleep(300000);
+    mvwprintw(title, 5, 0, "|_|   |_|   \\___/| |\\____)____)\\___)  /_/  \\_\\");
+    wrefresh(title);
+    refresh();
+    usleep(300000);
+    mvwprintw(title, 6, 0, "               (__/                           ");
+    wrefresh(title);
+    refresh();
+    sleep(1);
+    wattroff(title, COLOR_PAIR(1));
+    mvwprintw(title, 8, 0, "  [ by Davide Iacomino & Leonardo Mancini ]");
+    mvwprintw(title, 9, 0, "            !! WORKING TITLE !!");
+    wrefresh(title);
+    refresh();
+
+    sleep(1);
 
     w = getmaxx(belowTitle);
-    mvwprintw(belowTitle, 1, 1, "Press space to enter the Menu");
+    mvwprintw(belowTitle, 1, 1, "Press SPACE to enter the menu");
     wrefresh(belowTitle);
 
     return belowTitle;
@@ -33,9 +69,9 @@ WINDOW* Menu::printStartScreen(){
 //prints the main menu
 void Menu::printMenu() {
     clear();
-    mvprintw(0,0, "B2DAP");
-    mvprintw(1,0, "1. PLAY");
-    mvprintw(2,0, "0. QUIT");
+    mvprintw(0, 0, "B2DAP");
+    mvprintw(1, 0, "1. PLAY");
+    mvprintw(2, 0, "0. QUIT");
     refresh();
 }
 
@@ -50,10 +86,10 @@ void Menu::printEndScreen() {
 
 //prints a start game message
 void Menu::initGame() {
-    mvprintw(getmaxy(stdscr)/2, getmaxx(stdscr)/2-8, "The Game starts");
+    mvprintw(getmaxy(stdscr) / 2, getmaxx(stdscr) / 2 - 8, "The game starts");
     refresh();
     sleep(1);
-    mvprintw(getmaxy(stdscr)/2+1, getmaxx(stdscr)/2-18, "We wish for you to have a good time");
+    mvprintw(getmaxy(stdscr) / 2 + 1, getmaxx(stdscr) / 2 - 18, "We wish for you to have a good time");
     refresh();
     sleep(1);
     option = MenuOption::PLAY;
@@ -63,10 +99,10 @@ void Menu::initGame() {
 void Menu::menuLoop() {
     char choice;
     WINDOW* bt = printStartScreen();
-    do{
-        
+    do {
+
         choice = wgetch(bt);
-    }while(choice != ' ');
+    } while (choice != ' ');
     choice = '_';
 
     while (choice != '0' && choice != '1') {
@@ -93,44 +129,44 @@ void Menu::menuLoop() {
 }
 
 //prints commands and a description of the game elements
-void Menu::printHelpScreen(){
+void Menu::printHelpScreen() {
     clear();
     refresh();
     init_pair(6, COLOR_YELLOW, COLOR_BLACK);
     init_pair(7, COLOR_GREEN, COLOR_BLACK);
     init_pair(8, COLOR_MAGENTA, COLOR_BLACK);
     init_pair(9, COLOR_RED, COLOR_BLACK);
-    
-    mvprintw(getmaxy(stdscr)/8, getmaxx(stdscr)/8, "HELP LOG");
+
+    mvprintw(getmaxy(stdscr) / 8, getmaxx(stdscr) / 8, "HELP LOG");
     attron(COLOR_PAIR(6));
-    mvaddch(getmaxy(stdscr)/8+3, getmaxx(stdscr)/8, 'H');
+    mvaddch(getmaxy(stdscr) / 8 + 3, getmaxx(stdscr) / 8, 'H');
     attroff(COLOR_PAIR(6));
-    mvprintw(getmaxy(stdscr)/8+3, getmaxx(stdscr)/8+10, "this is you, the hero");
+    mvprintw(getmaxy(stdscr) / 8 + 3, getmaxx(stdscr) / 8 + 10, "this is you, the hero");
 
     attron(COLOR_PAIR(7));
-    mvprintw(getmaxy(stdscr)/8+5, getmaxx(stdscr)/8, "+ a A K");
+    mvprintw(getmaxy(stdscr) / 8 + 5, getmaxx(stdscr) / 8, "+ a A K");
     attroff(COLOR_PAIR(7));
-    mvprintw(getmaxy(stdscr)/8+5, getmaxx(stdscr)/8+10, "bonuses");
+    mvprintw(getmaxy(stdscr) / 8 + 5, getmaxx(stdscr) / 8 + 10, "bonuses");
 
     attron(COLOR_PAIR(8));
-    mvprintw(getmaxy(stdscr)/8+7, getmaxx(stdscr)/8, "| m W");
+    mvprintw(getmaxy(stdscr) / 8 + 7, getmaxx(stdscr) / 8, "| m W");
     attroff(COLOR_PAIR(8));
-    mvprintw(getmaxy(stdscr)/8+7, getmaxx(stdscr)/8+10, "Traps");
+    mvprintw(getmaxy(stdscr) / 8 + 7, getmaxx(stdscr) / 8 + 10, "traps");
 
     attron(COLOR_PAIR(9));
-    mvprintw(getmaxy(stdscr)/8+9, getmaxx(stdscr)/8, "$");
+    mvprintw(getmaxy(stdscr) / 8 + 9, getmaxx(stdscr) / 8, "$");
     attroff(COLOR_PAIR(8));
-    mvprintw(getmaxy(stdscr)/8+9, getmaxx(stdscr)/8+10, "XP points");
-    
-    mvprintw(getmaxy(stdscr)/8+14, getmaxx(stdscr)/2-12, "press space to continue");
+    mvprintw(getmaxy(stdscr) / 8 + 9, getmaxx(stdscr) / 8 + 10, "XP points");
 
-    mvprintw(getmaxy(stdscr)/8, getmaxx(stdscr)*3/4-29, "COMMANDS                    ");
-    mvprintw(getmaxy(stdscr)/8+3, getmaxx(stdscr)*3/4-29, "left-right movement:    a, d");
-    mvprintw(getmaxy(stdscr)/8+5, getmaxx(stdscr)*3/4-29, "jump:                  space");
-    mvprintw(getmaxy(stdscr)/8+7, getmaxx(stdscr)*3/4-29, "shoot:                     f");
-    mvprintw(getmaxy(stdscr)/8+9, getmaxx(stdscr)*3/4-29, "return to main screen:     0");
+    mvprintw(getmaxy(stdscr) / 8 + 14, getmaxx(stdscr) / 2 - 12, "Press SPACE to continue");
+
+    mvprintw(getmaxy(stdscr) / 8, getmaxx(stdscr) * 3 / 4 - 14, "COMMANDS                    ");
+    mvprintw(getmaxy(stdscr) / 8 + 3, getmaxx(stdscr) * 3 / 4 - 14, "left-right movement:    a, d");
+    mvprintw(getmaxy(stdscr) / 8 + 5, getmaxx(stdscr) * 3 / 4 - 14, "jump:                  space");
+    mvprintw(getmaxy(stdscr) / 8 + 7, getmaxx(stdscr) * 3 / 4 - 14, "shoot:                     f");
+    mvprintw(getmaxy(stdscr) / 8 + 9, getmaxx(stdscr) * 3 / 4 - 14, "return to main screen:     0");
     refresh();
-    while(getch()!=' ');
+    while (getch() != ' ');
     clear();
 }
 
